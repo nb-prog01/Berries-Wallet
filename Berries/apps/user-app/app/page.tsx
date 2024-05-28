765088cd1321a3cf98df7bb13b87324e11d64a4c
nb-prog01 <nirajbhadoria007@gmail.com>
2024-05-28 15:27:30 +0530

    re-structured

--- Berries/apps/user-app/app/page.tsx
+++ Berries/apps/user-app/app/page.tsx
@@ -1,11 +1,12 @@
-import { PrismaClient } from "@repo/db/client";
-
-const client=new PrismaClient();
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
 export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="text-2xl">
-      Yo!
-    </div>
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
   );
 }
